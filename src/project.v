3a3e8398732f19a8b2f96abf445c0cc954c2dc40
Henry Blount <henry.rev.024@gmail.com>
2026-05-18 17:36:32 -0400

    Refactor module and update input/output definitions

--- src/project.v
+++ src/project.v
@@ -1,15 +1,26 @@
 /*
  * Copyright (c) 2024 Henry Blount
  * SPDX-License-Identifier: Apache-2.0
- */
-module top (
-    input clk,
-    input pwm_in, //50Hz PWM input
-    input start, 
-    input tlm, //Telemetry 
-    output reg serial_out, //16bit D-Shot Frame
-    output reg busy
+*/
+
+`default_nettype none
+
+module tt_um_example (
+	input  wire pwm_in,   //50Hz PWM input
+	input  wire start,  
+	input  wire tlm, //Telemetry 
+	output wire serial_out //16bit D-Shot Frame
+	output wire  busy, 
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
     // PWM Pulse Width measurement
     reg [15:0] counter;
