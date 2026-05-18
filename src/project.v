2698bf783090637606dd6e5e488f07711f76ef8d
Henry Blount <henry.rev.024@gmail.com>
2026-05-18 17:58:10 -0400

    Refactor tt_um_boxcar_avg by removing unused code
    Removed unused wires and boxcar_core instantiation from tt_um_boxcar_avg module.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_boxcar_avg (
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,24 +15,6 @@ module tt_um_boxcar_avg (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-  wire _unused = &{ena, uio_in};
-
-  wire [7:0] avg_out;
-  wire       avg_valid;
-
-  boxcar_core u_core (
-        .clk      (clk),
-        .rst_n    (rst_n),
-        .data_in  ({2'b00, ui_in[5:0]}),
-        .sel      (ui_in[7:6]),
-        .data_out (avg_out),
-        .valid    (avg_valid)
-    );
-
-  assign uo_out  = avg_out;
-  assign uio_out = {7'b0, avg_valid};
-  assign uio_oe  = 8'b0000_0001;
 
 endmodule
 
